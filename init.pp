88aaeba3bffc78445ab864fa55de4a597aa469ad
tsibo <kaapo.koponen@myy.haaga-helia.fi>
2017-04-17 17:27:25 +0300

    init.pp lisätty

---
+++ init.pp
@@ -0,0 +1,15 @@
+class h2apache2{
+	package { 'apache2':
+		ensure => "installed",
+	}
+	file { '/var/www/html/index.html':
+                content => "vaihda muuta hauskaa tilalle kuin it works",
+        }
+	service { 'apache2':
+		ensure => 'running',
+		require => [
+		Package['apache2'],
+		File['/var/www/html/index.html'],
+		]
+	}
+}
