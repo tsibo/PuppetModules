add2124c47914ff4ad84c23c727bf42b788e4cd4
tsibo <kaapo.koponen@myy.haaga-helia.fi>
2017-04-17 19:50:18 +0300

    Puskua

--- homework3/modules/ssh/manifests/init.pp
+++ homework3/modules/ssh/manifests/init.pp
@@ -8,4 +8,9 @@ class ssh{
 		enable => "true",
 		require => Package["ssh"],
 	}
+	file {"/etc/ssh/sshd_config":
+		content => template('ssh/sshd_config.erb'),
+		require => Package["ssh"],
+		notify => Service ["sshd"],
+	}
 }
